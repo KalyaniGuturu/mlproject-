80414234af77c75bb8629ac89ef63483a61008ce
KalyaniGuturu <kalyaniguturu2006@gmail.com>
2026-06-02 21:53:56 +0530

    Hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_error
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression,Ridge,Lasso
 from sklearn.model_selection import RandomizedSearchCV
@@ -42,8 +42,7 @@ class ModelTrainer:
             )
             models={
                 'LinearRegression':LinearRegression(),
-                'Lasso':Lasso(),
-                'ridge':Ridge(),
+                'Gradient Boosting':GradientBoostingRegressor(),
                 'K-Neighbors Regressor':KNeighborsRegressor(),
                 'Decision Tree': DecisionTreeRegressor(),
                 'Random Forest regressor':RandomForestRegressor(),
@@ -51,8 +50,46 @@ class ModelTrainer:
                 'CatBoostRegressor':CatBoostRegressor(verbose=False),
                 'AdaBoostRegressor':AdaBoostRegressor(),
             }
+
+            params={
+                "Decision Tree":{
+                    'criterion':['squard_error','friedman_mse','absolute_error','poisson'],
+
+                },
+                "Random Forest regressor":{
+                    'n_estimators':[8,16,32,64,128,256]               
+                },
+    
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]         
+                },
+                "LinearRegression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+
+
+
+
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=params)
             
             ##to get the best model score from dict
             best_model_score=max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 import dill 
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
+
 
 from src.exception import CustomException
 
@@ -21,7 +22,7 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
     
         report={}
@@ -29,8 +30,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
-            model.fit(X_train,y_train)
+            para=param[list(models.keys())[i]]
+            # model.fit(X_train,y_train)
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
         
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
